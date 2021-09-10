1e2aa60847381280dad7bb5535a28d5944be00dd
HakureiHikaze <wgyaaa123@gmail.com>
2021-09-10 23:02:51 +0800

    dev

--- BinaryTree.hpp
+++ BinaryTree.hpp
@@ -10,8 +10,75 @@ namespace Hikaze {
     template<typename T>
     class BinaryTree : IBinaryTree<T>{
     public:
+        BinaryTree();
+        ~BinaryTree() override;
+
+        void preOrder() override;
+        void inOrder() override;
+        void postOrder() override;
+        void levelOrder() override;
+        void setElement(unsigned int,const T&) override;
     private:
         BTNode<T>* pRoot;
-
+        void delTree(BTNode<T>*);
+        void preOrder(BTNode<T>*);
+        void inOrder(BTNode<T>*);
+        void postOrder(BTNode<T>*);
+        void levelOrder(BTNode<T>*);
     };
+
+    template<typename T>
+    BinaryTree<T>::BinaryTree():pRoot(nullptr){}
+
+    template<typename T>
+    BinaryTree<T>::~BinaryTree() {
+        delTree(pRoot);
+    }
+
+    template<typename T>
+    void BinaryTree<T>::delTree(BTNode<T>* p) {
+        if(!p)return;
+        if(p->pLeft)delTree(p->pLeft);
+        if(p->pRight)delTree(p->pRight);
+        delete p;
+    }
+
+    template<typename T>
+    void BinaryTree<T>::preOrder() {
+        preOrder(pRoot);
+    }
+
+    template<typename T>
+    void BinaryTree<T>::preOrder(BTNode<T> * p) {
+        if(!p)return;
+        cout<<*p;
+        if(p->pLeft)preOrder(p->pLeft);
+        if(p->pRight)preOrder(p->pRight);
+    }
+
+    template<typename T>
+    void BinaryTree<T>::inOrder(BTNode<T> *) {
+        if(!p)return;
+        if(p->pLeft)preOrder(p->pLeft);
+        cout<<*p;
+        if(p->pRight)preOrder(p->pRight);
+    }
+
+    template<typename T>
+    void BinaryTree<T>::postOrder(BTNode<T> *) {
+        if(!p)return;
+        if(p->pLeft)preOrder(p->pLeft);
+        if(p->pRight)preOrder(p->pRight);
+        cout<<*p;
+    }
+
+    template<typename T>
+    void BinaryTree<T>::inOrder() {
+        inOrder(pRoot);
+    }
+
+    template<typename T>
+    void BinaryTree<T>::postOrder() {
+        postOrder(pRoot);
+    }
 }
--- NodeBinaryTree.hpp
+++ NodeBinaryTree.hpp
@@ -4,10 +4,25 @@
 
 #pragma once
 
+#include <iostream>
+
 namespace Hikaze{
     template<typename T>
     struct BTNode{
         T data;
         BTNode<T>* pLeft,* pRight;
+    public:
+        BTNode():pLeft(nullptr),pRight(nullptr),data(T()){}
+        template<typename DT>
+        friend std::ostream& operator<<(std::ostream&, BTNode<DT>*);
     };
+
+    template<typename DT>
+    std::ostream &operator<<(std::ostream & os, BTNode<DT> * pNode) {
+        os<<"\nAddr:\t"<<(void*)pNode<<
+        "\nData:\t"<<pNode->data<<
+        "\nLeft:\t"<<(void*)pNode->pLeft<<
+        "\nRight:\t"<<(void*)pNode->pRight<<std::endl;
+        return os;
+    }
 }
