cb209d40eaecc2e70b43396dd65c78743a569a66
HakureiHikaze <wgyaaa123@gmail.com>
2021-09-25 20:27:39 +0800

    dev

--- main.cpp
+++ main.cpp
@@ -1,16 +1,3 @@
-#include <iostream>
-#include <cmath>
-#include "SeqBTree.hpp"
-using namespace std;
 int main() {
-    Hikaze::SeqBTree<char> t;
-    double i = 0.0;
-    int j = 0;
-    while(i<100.0){
-        t.setElement(j, '1');
-        i+=1.0;
-        j++;
-    }
-    t.preOrder();
     return 0;
 }
